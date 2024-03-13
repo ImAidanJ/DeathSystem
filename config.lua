8d74e63d2feb5a155e2c666a13ddeef451649a9c
ImAidanJ <realAidanJ@gmail.com>
2024-03-13 09:26:33 -0400

    started work on death system

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config {}
+
+Config.Revive {
+EnableAcePerms = true, -- Enable perms for admins to revive instanly or to revive others.
+AcePermString = "AJ.Revive", -- The ace perm string that will be added to you server.cfg
+
+ReviveTime = 120, -- The amount of time until revive becomes avalible
+}
+
+Config.Respawn {
+EnableAcePerms = true, -- Enable perms for admins to respawn instanly or to respawn others.
+AcePermString = "AJ.Respawn", -- The ace perm string that will be added to you server.cfg
+
+RespawnTime = 60, -- The amount of time until respawn becomes avalible
+}
+
+Config.Locations {
+
+}
