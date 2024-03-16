249b9b6f339cd35bb6f64e874f5ce8649d41905f
ImAidanJ <realAidanJ@gmail.com>
2024-03-16 12:37:58 -0400

    Added fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ImAidanJ'
+description 'FiveM Death System'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client/*.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server/*.lua'
+}
